aeb4c6031f50b7dce43e68909a67d765fd84d8f5
touraye <touraye07@gmail.com>
2023-08-14 19:03:18 +0000

    created unicafe step2

--- part1/unicafe/src/App.jsx
+++ part1/unicafe/src/App.jsx
@@ -4,12 +4,22 @@ function App() {
   const [good, setGood] = useState(0)
   const [neutral, setNeutral] = useState(0)
   const [ bad, setBad ] = useState( 0 )
+  const [all, setAll] = useState(0);
   
-  const handleGood = () => setGood(good + 1)
+  const handleGood = () => {
+    setGood( good + 1 )
+    setAll(all+1)
+  }
 
-   const handleNeutral = () => setNeutral(neutral + 1)
+  const handleNeutral = () => {
+    setNeutral( neutral + 1 )
+    setAll(all + 1)
+  }
   
-   const handleBad = () => setBad(bad + 1)
+  const handleBad = () => {
+    setBad( bad + 1 )
+    setAll(all + 1)
+  }
 
   return (
 		<>
@@ -20,9 +30,12 @@ function App() {
 				<button onClick={handleBad}>bad</button>
 			</div>
 
-			<p>{good}</p>
-			<p>{neutral}</p>
-			<p>{bad}</p>
+			<p>good {good}</p>
+			<p>neutral {neutral}</p>
+			<p>bad {bad}</p>
+			<p>all {all}</p>
+			<p>average {all / 3}</p>
+			<p>positive {((good + neutral) / all) * 100}%</p>
 		</>
 	)
 }
