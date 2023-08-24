0ca6ba1d2676853e895abaddace995d2dc543fa4
touraye <touraye07@gmail.com>
2023-08-24 11:31:45 +0000

    phonebook backend step9

--- part3/phonebook-backend/index.js
+++ part3/phonebook-backend/index.js
@@ -115,7 +115,7 @@ const unknownEndpoint = (request, response) => {
 
 app.use(unknownEndpoint)
 
-const PORT = 3001
+const PORT = process.env.PORT || 3001
 app.listen( PORT, () => {
     console.log(`Server stated on ${PORT}`);
 })
