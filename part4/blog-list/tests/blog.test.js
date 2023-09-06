8fef7437f4461b43076c9b88c28432767b580395
touraye <touraye07@gmail.com>
2023-09-06 08:09:55 +0000

    helper functions and unit tests, step2

--- part4/blog-list/tests/blog.test.js
+++ part4/blog-list/tests/blog.test.js
@@ -12,4 +12,55 @@ test('dummy returns one', () => {
 
 	const result = listHelper.dummy(blogs)
 	expect(result).toBe(1)
+} )
+
+describe( 'blog list test', () => {
+	test( 'of empty list is zero', ( ) => { 
+		const blogs = []
+
+		const result = listHelper.totalLikes(blogs)
+		expect(result).toBe(0)
+	} )
+
+	test('when list has only one blog equals to the likes of that', () => {
+		const blogs = [
+			{
+				author: 'JS Mastery',
+				url: 'jsmastery.com/blog/server-client-rendering',
+				likes: 5,
+				id: '64f70a264b7256df4a522007',
+			},
+		]
+
+		const result = listHelper.totalLikes(blogs)
+		expect(result).toBe(5)
+	})
+	
+	test( 'of bigger list is calculated right', () => {
+		const blogs = [
+			{
+			"author": "JS Mastery",
+			"url": "jsmastery.com/blog/nestjs-app-router",
+			"likes": 10,
+			"id": "64f709954b7256df4a522004"
+			},
+			{
+				"author": "JS Mastery",
+				"url": "jsmastery.com/blog/server-client-rendering",
+				"likes": 5,
+				"id": "64f70a264b7256df4a522007"
+			},
+			{
+				"author": "JS Mastery",
+				"url": "jsmastery.com/blog/server-client-rendering",
+				"likes": 5,
+				"id": "64f71c43759800ce692902e9"
+			}
+		]
+	
+		const result = listHelper.totalLikes(blogs)
+		expect(result).toBe(20)
+	} )		
+	
 })
+
--- part4/blog-list/utils/list_helper.js
+++ part4/blog-list/utils/list_helper.js
@@ -2,6 +2,20 @@ const dummy = (blogs) => {
     return 1
 }
 
+const totalLikes = ( blogs ) => {
+    let numberOfLikes = 0
+
+    for (let blog of blogs) {
+			numberOfLikes += blog.likes
+		}
+   
+    return blogs.length === 0 
+        ? 0 :
+        numberOfLikes
+        
+}
+
 module.exports = {
-	dummy,
+    dummy,
+    totalLikes
 }
