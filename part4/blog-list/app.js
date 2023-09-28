f86219deb4ffec878fb613fd206f8f9751edd8fb
touraye <touraye07@gmail.com>
2023-09-28 09:02:02 +0000

    bloglist expansion, step8

--- part4/blog-list/app.js
+++ part4/blog-list/app.js
@@ -25,7 +25,8 @@ mongoose
 app.use(cors())
 app.use(express.static('build'))
 app.use(express.json())
-app.use(middleware.requestLogger)
+app.use( middleware.requestLogger )
+app.use(middleware.tokenExtractor)
 
 app.use('/api/blogs', blogsRouter)
 app.use( '/api/users', usersRouter )
--- part4/blog-list/controllers/blogs.js
+++ part4/blog-list/controllers/blogs.js
@@ -14,19 +14,11 @@ blogsRouter.get('/', async (request, response, next) => {
 	}
 } )
 
-const getTokenFrom = (request) => {
-	const authorization = request.get('authorization')
-	if (authorization && authorization.startsWith('Bearer ')) {
-		return authorization.replace('Bearer ', '')
-	}
-	return null
-}
-
 blogsRouter.post('/', async (request, response, next) => {
 	const blog = new Blog( request.body )		
 	
 	try {
-		const decodedToken = jwt.verify(getTokenFrom(request), process.env.SECRET)
+		const decodedToken = jwt.verify(request.token, process.env.SECRET)
 			if (!decodedToken.id) {
 				return response.status(401).json({ error: 'token invalid' })
 			}
